d67965a01f9a66f10f1e3905ba2595a378c8a54a
Prantik Paul <pcp.3240@gmail.com>
2025-01-08 05:41:01 +0600

    2nd

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,16 @@
+{{-- <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Page Title' }}</title>
+    </head>
+    <body>
+        {{ $slot }}
+    </body>
+</html> --}}
+
 <!DOCTYPE html>
 <html class="no-js" lang="en">
 
@@ -571,7 +584,7 @@
         </div>
     </div>        
 
-    @livewire('HomeComponent')
+    {{ $slot }}
 
     <footer class="main">
         <section class="newsletter p-30 text-white wow fadeIn animated">
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', HomeComponent::class);
